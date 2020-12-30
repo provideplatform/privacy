c1cfd6edf0a626e677233a15bf1db5a23ac7ca2f
Kyle Thomas <kyle@provide.services>
2020-12-30 05:34:11 -0500

    Cleanup ops/

--- ops/run_integration_tests.sh
+++ ops/run_integration_tests.sh
@@ -2,6 +2,5 @@
 
 docker-compose -f ./ops/docker-compose.yml build --no-cache privacy
 docker-compose -f ./ops/docker-compose.yml up -d
-sleep 5
 TAGS=integration ./ops/run_local_tests.sh
 docker-compose -f ./ops/docker-compose.yml down

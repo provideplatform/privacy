a7c588dfab0926603e45549fcf5cecd9846d5efe
Myron Rotter <myronrotter01@t-online.de>
2021-01-25 13:19:47 +0100

    Add tests for cubic circuit for completeness

---
+++ zkp/lib/circuits/gnark/cubic_test.go
@@ -0,0 +1,35 @@
+package gnark
+
+import (
+	"testing"
+
+	"github.com/consensys/gnark/backend/groth16"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gurvy"
+)
+
+func TestCubicEquation(t *testing.T) {
+	assert := groth16.NewAssert(t)
+
+	var cubicCircuit CubicCircuit
+
+	// compiles our circuit into a R1CS
+	r1cs, err := frontend.Compile(gurvy.BN256, &cubicCircuit)
+	assert.NoError(err)
+
+	{
+		var witness CubicCircuit
+		witness.X.Assign(42)
+		witness.Y.Assign(42)
+
+		assert.ProverFailed(r1cs, &witness)
+	}
+
+	{
+		var witness CubicCircuit
+		witness.X.Assign(3)
+		witness.Y.Assign(35)
+		assert.ProverSucceeded(r1cs, &witness)
+	}
+
+}

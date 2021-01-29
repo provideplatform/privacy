f3946f7c28eb846949d2f2f03ce5a0fb1b8b2e16
Myra DeMere <myrademere@gmail.com>
2021-01-28 17:55:38 -0700

    Update merkle comparison test

--- test/e2e_test.go
+++ test/e2e_test.go
@@ -5,6 +5,7 @@ package test
 import (
 	"bytes"
 	"encoding/hex"
+	"math"
 	"math/big"
 	"testing"
 	"time"
@@ -229,61 +230,72 @@ func TestBaselineRollup(t *testing.T) {
 	t.Logf("added proof to merkle tree at index/hash: %v / %v", index, hash)
 }
 
-func TestMerkleImplementationsRootHashCalculationMismatch(t *testing.T) {
+func TestMerkleImplementations(t *testing.T) {
 	hFunc := mimc.NewMiMC("seed")
 
 	proofs := []string{
 		"aa5500ca9af223afdec21989169de5c63938274908f09ee85a233fd1a7396bba89ea271a41a7d38014dfffbdcbe806d0726c5dc4eef7f178ea52de45852697d51f2c74152e1fbbed79ebdfd1235788ea2b1637e6ed49a33a05653133e21a5cfdaa86f1acc9588b17838f8da88a5398cb324b1289aa7759457ddccddf53ee1ca9",
 		"9f2becacfe12908f1766b00ed6c7c1d7aa0aa65d2c9651d3aa70b30d25a884aeac1e50a5848f3c2ce9877a690d56801da287224ec2be2e1f26fd73b519fa1b7a19980d9b770a872d03612bd217ea4ff2f6a3bef527997f1eea30098e6772ca8cb033c3f019ce79f794419d2db6039855759e29addc72836fb3a99379e4d5e572",
+		"0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
 	}
 
-	hashes := make([]byte, hFunc.Size()*len(proofs))
+	var buf bytes.Buffer
+	segmentSize := hFunc.Size()
+
+	// Pad out tree with default hashes if proof count is not a power of two
+	paddedSize := int(math.Exp2(math.Ceil(math.Log2(float64(len(proofs))))))
 	for i := 0; i < len(proofs); i++ {
 		hFunc.Reset()
 		hFunc.Write([]byte(proofs[i]))
 		sum := hFunc.Sum(nil)
-		copy(hashes[i*hFunc.Size():(i+1)*hFunc.Size()], sum)
+		buf.Write(sum)
+	}
+	for i := len(proofs); i < paddedSize; i++ {
+		hFunc.Reset()
+		hFunc.Write([]byte{})
+		sum := hFunc.Sum(nil)
+		buf.Write(sum)
 	}
 
-	var buf bytes.Buffer
-	_, err := buf.Write(hashes)
+	proofIndex := uint64(0)
+	merkleRoot, proofSet, numLeaves, err := gnark_merkle.BuildReaderProof(&buf, hFunc, segmentSize, proofIndex)
 	if err != nil {
-		t.Error("failed to write hashes to buffer")
-		return
-	}
-	merkleRoot, proofSet, numLeaves, err := gnark_merkle.BuildReaderProof(&buf, hFunc, hFunc.Size(), 0)
-	if err != nil {
-		t.Error("failed to build reader proof")
+		t.Error("failed to build merkle proof")
 		return
 	}
 
-	t.Logf("proofset: %s, %s", hex.EncodeToString(proofSet[0]), hex.EncodeToString(proofSet[1]))
-	t.Logf("numLeaves: %d", numLeaves)
+	proofVerified := gnark_merkle.VerifyProof(hFunc, merkleRoot, proofSet, proofIndex, numLeaves)
+	if !proofVerified {
+		t.Error("failed to verify merkle proof")
+		return
+	}
 
 	t.Log("gnark merkle implementation:")
-	hashString := []string{
-		hex.EncodeToString(hashes[:32]),
-		hex.EncodeToString(hashes[32:]),
-	}
-	t.Logf("hashes: %s, %s root: %s", hashString[0], hashString[1], hex.EncodeToString(merkleRoot))
+	t.Logf("root: %s", hex.EncodeToString(merkleRoot))
 
 	t.Log("provide merkle implementation:")
 	tr := merkletree.NewMerkleTree(hFunc)
-	hashMatches := []bool{false, false}
 
-	index, hash := tr.Add([]byte(proofs[0]))
-	hashMatches[0] = hash == hashString[0]
-	t.Logf("index/hash: %d / %s", index, hash)
-	index, hash = tr.Add([]byte(proofs[1]))
-	hashMatches[1] = hash == hashString[1]
-	t.Logf("index/hash: %d / %s", index, hash)
-	// root := tr.Recalculate()
+	for i := 0; i < len(proofs); i++ {
+		hFunc.Reset()
+		hFunc.Write([]byte(proofs[i]))
+		sum := hFunc.Sum(nil)
+		index, hash := tr.RawAdd(sum)
+		t.Logf("index/hash: %d / %s", index, hash)
+	}
+	for i := len(proofs); i < paddedSize; i++ {
+		hFunc.Reset()
+		hFunc.Write([]byte{})
+		sum := hFunc.Sum(nil)
+		index, hash := tr.RawAdd(sum)
+		t.Logf("index/hash: %d / %s", index, hash)
+	}
+	tr.Recalculate()
 	root := tr.Root()
 	t.Logf("root: %s", root)
-	t.Logf("hash 0 matches: %v, hash 1 matches: %v, root matches: %v", hashMatches[0], hashMatches[1], root == hex.EncodeToString(merkleRoot))
 
-	if root == hex.EncodeToString(merkleRoot) {
-		t.Error("calculated root for each implementation should not match for reasons to be investigated")
+	if root != hex.EncodeToString(merkleRoot) {
+		t.Error("calculated root for each implementation should match for proof counts which are powers of two")
 		return
 	}
 }

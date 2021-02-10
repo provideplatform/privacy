7d118e0a1d679c3540702134a64c62ef44472349
Myron Rotter <myronrotter01@t-online.de>
2021-02-10 16:55:02 +0100

    Move tests to ./test; Add tests for supported curves

--- test/zkp/lib/circuits/gnark/mimc_test.go
+++ test/zkp/lib/circuits/gnark/mimc_test.go
@@ -7,25 +7,26 @@ import (
 
 	"github.com/consensys/gnark/backend/groth16"
 	"github.com/consensys/gnark/frontend"
+	libgnark "github.com/provideapp/privacy/zkp/lib/circuits/gnark"
 )
 
 func TestPreimage(t *testing.T) {
 	assert := groth16.NewAssert(t)
 
-	var mimcCircuit MimcCircuit
+	var mimcCircuit libgnark.MimcCircuit
 
 	r1cs, err := frontend.Compile(gurvy.BN256, &mimcCircuit)
 	assert.NoError(err)
 
 	{
-		var witness MimcCircuit
+		var witness libgnark.MimcCircuit
 		witness.Hash.Assign(42)
 		witness.PreImage.Assign(42)
 		assert.ProverFailed(r1cs, &witness)
 	}
 
 	{
-		var witness MimcCircuit
+		var witness libgnark.MimcCircuit
 		witness.PreImage.Assign(35)
 		witness.Hash.Assign("16130099170765464552823636852555369511329944820189892919423002775646948828469")
 		assert.ProverSucceeded(r1cs, &witness)

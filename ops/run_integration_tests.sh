3d9efbc24fc56cbcdd797f20e3974d3257a45e8a
Myra DeMere <myrademere@gmail.com>
2021-03-08 12:09:41 -0700

    Update names for docker compose integration

--- ops/run_integration_tests.sh
+++ ops/run_integration_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker-compose -f ./ops/docker-compose.yml build --no-cache privacy
-docker-compose -f ./ops/docker-compose.yml up -d
+docker-compose -f ./ops/docker-compose-integration.yml build --no-cache privacy
+docker-compose -f ./ops/docker-compose-integration.yml up -d
 TAGS=integration ./ops/run_local_tests.sh
-docker-compose -f ./ops/docker-compose.yml down
+docker-compose -f ./ops/docker-compose-integration.yml down

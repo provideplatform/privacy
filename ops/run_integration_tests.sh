8644668bfcfc1a1f8dcec5f411a40986d74dc5f2
Kyle Thomas <kyle@provide.services>
2021-03-08 14:55:20 -0500

    Update hostnames in integration docker-compose

--- ops/run_integration_tests.sh
+++ ops/run_integration_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker-compose -f ./ops/docker-compose-integration.yml build --no-cache privacy
+docker-compose -f ./ops/docker-compose-integration.yml build --no-cache
 docker-compose -f ./ops/docker-compose-integration.yml up -d
 TAGS=integration ./ops/run_local_tests.sh
 docker-compose -f ./ops/docker-compose-integration.yml down

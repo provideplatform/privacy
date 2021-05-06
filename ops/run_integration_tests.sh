16333296ec7748d49434b848cd9663c374ea5ec3
Myra DeMere <myrademere@gmail.com>
2021-05-06 13:17:59 -0600

    WIP for harness

--- ops/run_integration_tests.sh
+++ ops/run_integration_tests.sh
@@ -6,5 +6,5 @@ docker build -f test/Dockerfile -t privacy-tests .
 docker-compose -f ./ops/docker-compose-integration.yml build --no-cache
 docker-compose -f ./ops/docker-compose-integration.yml up -d
 # TAGS=integration ./ops/run_local_tests.sh
-docker run privacy-tests
+docker run -e "TAGS=integration" privacy-tests
 docker-compose -f ./ops/docker-compose-integration.yml down

856edcb7a811c8a461defeb7a6238af6d7937471
Myra DeMere <myrademere@gmail.com>
2021-06-03 18:13:21 -0600

    Fix cubic plonk test

--- test/zkp/lib/circuits/gnark/cubic_test.go
+++ test/zkp/lib/circuits/gnark/cubic_test.go
@@ -6,7 +6,8 @@ import (
 	"testing"
 
 	"github.com/consensys/gnark-crypto/ecc"
-	kzgcommitment_bn254 "github.com/consensys/gnark-crypto/ecc/bn254/fr/polynomial/kzg"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr/polynomial/kzg"
 	mockcommitment_bn254 "github.com/consensys/gnark-crypto/ecc/bn254/fr/polynomial/mockcommitment"
 	"github.com/consensys/gnark/backend"
 	"github.com/consensys/gnark/backend/groth16"
@@ -130,18 +131,19 @@ func TestCubicEquationPlonkElaboratedWithSpecifiedKzgCommitment(t *testing.T) {
 	sparseR1cs, err := frontend.Compile(ecc.BN254, backend.PLONK, &cubicCircuit)
 	assert.NoError(err)
 
+	var alpha fr.Element
+	alpha.SetRandom()
+	kate := kzg.NewScheme(64, alpha)
+
 	{
 		var witness libgnark.CubicCircuit
 		witness.X.Assign(3)
 		witness.Y.Assign(35)
 
-		polynomialCommitment := &kzgcommitment_bn254.Scheme{}
-
-		publicData, err := plonk.Setup(sparseR1cs, polynomialCommitment, &witness)
+		publicData, err := plonk.Setup(sparseR1cs, kate, &witness)
 		assert.NoError(err, "Generating public data should not have failed")
 
 		proof, err := plonk.Prove(sparseR1cs, publicData, &witness)
-		// FIXME-- this gets error ":"the size of the polynomials exceeds the capacity of the SRS" from gnark-crypto
 		assert.NoError(err, "Proving with good witness should not output an error")
 
 		err = plonk.Verify(proof, publicData, &witness)

92ccae702a5aa96861a9471483d46bc42f96a125
Myra DeMere <myrademere@gmail.com>
2021-07-27 18:45:09 -0600

    Add merkle circuit key size unit test

--- test/zkp/lib/circuits/gnark/key_size_test.go
+++ test/zkp/lib/circuits/gnark/key_size_test.go
@@ -27,12 +27,14 @@ import (
 	frbw6761 "github.com/consensys/gnark-crypto/ecc/bw6-761/fr"
 	eddsabw6761 "github.com/consensys/gnark-crypto/ecc/bw6-761/twistededwards/eddsa"
 
+	"github.com/consensys/gnark-crypto/accumulator/merkletree"
 	"github.com/consensys/gnark-crypto/hash"
 	"github.com/consensys/gnark-crypto/signature"
 	"github.com/consensys/gnark/backend"
 	"github.com/consensys/gnark/backend/groth16"
 	"github.com/consensys/gnark/backend/plonk"
 
+	"github.com/consensys/gnark/std/accumulator/merkle"
 	"github.com/consensys/gnark/std/algebra/twistededwards"
 	"github.com/consensys/gnark/std/hash/mimc"
 	"github.com/consensys/gnark/std/signature/eddsa"
@@ -585,5 +587,111 @@ func TestKeySizesByEddsaWitnessSize(t *testing.T) {
 
 		err = plonk.Verify(proof, vk, &witness)
 		assert.NoError(err, "Verifying correct proof with correct witness should not output an error")
+	}
+}
+
+func TestKeySizesForMerkleCircuit(t *testing.T) {
+	assert := plonk.NewAssert(t)
+
+	confs := map[ecc.ID]hash.Hash{
+		ecc.BN254:     hash.MIMC_BN254,
+		ecc.BLS12_381: hash.MIMC_BLS12_381,
+		ecc.BLS12_377: hash.MIMC_BLS12_377,
+		ecc.BW6_761:   hash.MIMC_BW6_761,
+		ecc.BLS24_315: hash.MIMC_BLS24_315,
+	}
+
+	for id, h := range confs {
+		for count := 8192; count <= 10000; count *= 2 {
+			hFunc := h.New("seed")
+
+			var pfbuf bytes.Buffer
+
+			proofString := "aa5500ca9af223afdec21989169de5c63938274908f09ee85a233fd1a7396bba89ea271a41a7d38014dfffbdcbe806d0726c5dc4eef7f178ea52de45852697d51f2c74152e1fbbed79ebdfd1235788ea2b1637e6ed49a33a05653133e21a5cfdaa86f1acc9588b17838f8da88a5398cb324b1289aa7759457ddccddf53ee1ca9"
+
+			for i := 0; i < count; i++ {
+				hFunc.Reset()
+				hFunc.Write([]byte(proofString))
+				sum := hFunc.Sum(nil)
+				pfbuf.Write(sum)
+			}
+
+			proofIndex := uint64(0)
+			segmentSize := hFunc.Size()
+			merkleRoot, proofSet, numLeaves, err := merkletree.BuildReaderProof(&pfbuf, hFunc, segmentSize, proofIndex)
+			if err != nil {
+				t.Errorf("failed to build merkle proof; %s", err.Error())
+				return
+			}
+
+			proofVerified := merkletree.VerifyProof(hFunc, merkleRoot, proofSet, proofIndex, numLeaves)
+			if !proofVerified {
+				t.Errorf("failed to verify merkle proof; %s", err.Error())
+				return
+			}
+
+			var circuit, witness libgnark.BaselineRollupCircuit
+
+			circuit.Proofs = make([]frontend.Variable, len(proofSet))
+			circuit.Helpers = make([]frontend.Variable, len(proofSet)-1)
+
+			r1cs, err := frontend.Compile(id, backend.PLONK, &circuit)
+			assert.NoError(err)
+
+			kzgSRS, err := getKzgScheme(r1cs)
+			assert.NoError(err, "Getting KZG scheme should not have failed")
+
+			merkleProofHelper := merkle.GenerateProofHelper(proofSet, proofIndex, numLeaves)
+
+			witness.Proofs = make([]frontend.Variable, len(proofSet))
+			witness.Helpers = make([]frontend.Variable, len(proofSet)-1)
+			witness.RootHash.Assign(merkleRoot)
+			for i := 0; i < len(proofSet); i++ {
+				witness.Proofs[i].Assign(proofSet[i])
+
+				if i < len(proofSet)-1 {
+					witness.Helpers[i].Assign(merkleProofHelper[i])
+				}
+			}
+
+			pk, vk, err := plonk.Setup(r1cs, kzgSRS)
+			assert.NoError(err, "Generating public data should not have failed")
+
+			buf := new(bytes.Buffer)
+			_, err = pk.(io.WriterTo).WriteTo(buf)
+			if err != nil {
+				t.Errorf("failed to write proving key to buffer")
+			}
+
+			pkSize := buf.Len()
+
+			proof, err := plonk.Prove(r1cs, pk, &witness)
+			assert.NoError(err, "Proving with good witness should not output an error")
+
+			buf.Reset()
+			_, err = vk.(io.WriterTo).WriteTo(buf)
+			if err != nil {
+				t.Errorf("failed to write verifying key to buffer")
+			}
+
+			vkSize := buf.Len()
+
+			buf.Reset()
+			_, err = proof.(io.WriterTo).WriteTo(buf)
+			if err != nil {
+				t.Errorf("failed to write proof to buffer")
+			}
+
+			t.Logf("chunks: %5d | set sz: %5d | curve: %9s | pk size: %10d | vk size: %4d | pf size: %4d\n", count, len(proofSet), id.String(), pkSize, vkSize, buf.Len())
+
+			var publicWitness libgnark.BaselineRollupCircuit
+
+			publicWitness.Proofs = make([]frontend.Variable, len(proofSet))
+			publicWitness.Helpers = make([]frontend.Variable, len(proofSet)-1)
+			publicWitness.RootHash.Assign(merkleRoot)
+
+			err = plonk.Verify(proof, vk, &publicWitness)
+			assert.NoError(err, "Verifying correct proof with correct witness should not output an error")
+		}
 	}
 }

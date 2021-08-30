e0976a2d671fa709f8b966af33a69b401a587458
Myra DeMere <myrademere@gmail.com>
2021-08-30 14:10:03 -0600

    Make ceremony content agnostic

--- test/ceremony/ceremony_test.go
+++ test/ceremony/ceremony_test.go
@@ -3,21 +3,18 @@
 package test
 
 import (
-	"bytes"
 	"fmt"
 	"math/big"
 	"testing"
 
-	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/provideplatform/privacy/ceremony"
 )
 
-func setupTestMPCs(t *testing.T, mpcs *[]*ceremony.Ceremony, parties [][]byte, blockID uint64) error {
+func setupTestMPCs(t *testing.T, mpcs *[]*ceremony.Ceremony, parties []string, blockID uint64) error {
 	for i := 0; i < len(parties); i++ {
 		mpc := ceremony.CeremonyFactory(parties, &ceremony.CeremonyConfig{
 			Block:           &blockID,
 			ExpectedEntropy: 32 * (len(parties) + 1),
-			Index:           i,
 			WordSize:        32,
 		})
 
@@ -48,45 +45,17 @@ func addPartiesToTestMPCs(t *testing.T, mpcs []*ceremony.Ceremony) error {
 	partyCount := len(mpcs)
 	for i := 0; i < partyCount; i++ {
 		for j := 0; j < partyCount; j++ {
-			if i == j {
-				continue
-			}
-			mpcs[i].AddParty(mpcs[j])
+			mpcs[i].AddParty(j, mpcs[j])
 		}
 	}
 
 	return nil
 }
 
-func validateSRS(t *testing.T, mpcs []*ceremony.Ceremony) error {
-	// TODO: get correct size for SRS from circuit API?
-	sizeForSRS := uint64(500)
-
-	srs, err := mpcs[0].GenerateSRS(sizeForSRS, ecc.BN254)
-	if err != nil {
-		return fmt.Errorf("error generating initial SRS; %s", err.Error())
-	}
-
-	srsBuf := new(bytes.Buffer)
-	_, err = srs.WriteTo(srsBuf)
-	if err != nil {
-		return fmt.Errorf("error writing initial SRS to bytes buffer; %s", err.Error())
-	}
-
-	for _, mpc := range mpcs {
-		testSRS, err := mpc.GenerateSRS(sizeForSRS, ecc.BN254)
-		if err != nil {
-			return fmt.Errorf("error generating test SRS; %s", err.Error())
-		}
-
-		testSRSBuf := new(bytes.Buffer)
-		_, err = testSRS.WriteTo(testSRSBuf)
-		if err != nil {
-			return fmt.Errorf("error writing test SRS to bytes buffer; %s", err.Error())
-		}
-
-		if !bytes.Equal(srsBuf.Bytes(), testSRSBuf.Bytes()) {
-			return fmt.Errorf("test SRS does not match initial SRS")
+func validateEntropy(t *testing.T, mpcs []*ceremony.Ceremony) error {
+	for i := 1; i < len(mpcs); i++ {
+		if !mpcs[i-1].CompareEntropy(mpcs[i]) {
+			return fmt.Errorf("entropy from mpc %d does not match mpc %d", i-1, i)
 		}
 	}
 
@@ -99,12 +68,12 @@ func TestCeremonySRSGeneration(t *testing.T) {
 	// TODO: retrieve block ID properly
 	blockID := uint64(123456)
 	const partyCount = 5
-	parties := make([][]byte, 0)
+	parties := make([]string, 0)
 
 	i := new(big.Int)
 	for party := int64(0); party < int64(partyCount); party++ {
 		i.SetInt64(party)
-		parties = append(parties, i.Bytes())
+		parties = append(parties, i.String())
 	}
 
 	err := setupTestMPCs(t, &mpcs, parties, blockID)
@@ -121,11 +90,11 @@ func TestCeremonySRSGeneration(t *testing.T) {
 		return
 	}
 
-	err = validateSRS(t, mpcs[:])
+	err = validateEntropy(t, mpcs[:])
 	if err != nil {
-		t.Errorf("error validating SRS for test MPCs; %s", err.Error())
+		t.Errorf("error validating entropy for test MPCs; %s", err.Error())
 		return
 	}
 
-	t.Log("all generated SRS are valid")
+	t.Log("all calculated entropy values are valid")
 }
